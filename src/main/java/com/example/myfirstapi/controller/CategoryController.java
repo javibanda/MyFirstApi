6516bde9836f5e41dd959617c62d494a6f1f7fa1
javi <narutojavibanda@gmail.com>
2021-07-07 00:33:41 +0200

    Create fun updateCategory (PUT)

--- src/main/java/com/example/myfirstapi/controller/CategoryController.java
+++ src/main/java/com/example/myfirstapi/controller/CategoryController.java
@@ -5,12 +5,10 @@ import com.example.myfirstapi.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @RestController
@@ -22,7 +20,6 @@ public class CategoryController {
     @GetMapping("/category")
     public ResponseEntity<?> getAllCategory(){
         List<Category> categories = categoryRepository.findAll();
-
         if (categories.isEmpty()){
             return ResponseEntity.notFound().build();
         }else{
@@ -32,21 +29,53 @@ public class CategoryController {
 
     @PostMapping("/category")
     public ResponseEntity<?> createNewCategory(@RequestBody Category newCategory){
-        if (categoryExists(newCategory.getName())){
+        if (duplicateCategory(newCategory)){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }else if ((newCategory.getName().length() < 3)){
+        }else if (isShortName(newCategory)){
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }else{
             return new ResponseEntity<>(saveCategory(newCategory), HttpStatus.CREATED);
         }
     }
 
-    private boolean categoryExists(String name){
-        List<Category> categoryList  = categoryRepository.findByName(name);
-        return !categoryList.isEmpty();
+    @PutMapping("/category/{id}")
+    public ResponseEntity<?> updateCategory(@PathVariable("id") long id, @RequestBody Category category){
+        if (duplicateCategory(category)) {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }else if (isShortName(category)){
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }else if (categoryNotExists(id)){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }else {
+            return new ResponseEntity<>(changeNameCategory(category, id), HttpStatus.OK);
+        }
+    }
+
+    private boolean isShortName(Category category){
+        return  (category.getName().length() < 3);
+    }
+
+    private boolean duplicateCategory(Category category){
+        Optional<Category> categoryData = categoryRepository.findByName(category.getName());
+        return categoryData.isPresent();
+    }
+
+    private boolean categoryExists(Long id){
+        Optional<Category> categoryData = categoryRepository.findById(id);
+        return categoryData.isPresent();
+    }
+
+    private boolean categoryNotExists(Long id){
+        return !categoryExists(id);
     }
 
     private Category saveCategory(Category category){
         return categoryRepository.save(new Category(category.getName()));
+    }
+
+    private Category changeNameCategory(Category category, Long id){
+        Category categoryChanged = categoryRepository.findById(id).get();
+        categoryChanged.setName(category.getName());
+        return categoryRepository.save(categoryChanged);
     }
 }
