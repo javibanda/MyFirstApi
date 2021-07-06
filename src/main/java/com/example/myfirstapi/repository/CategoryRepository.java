2fd86185d00eefacb78f5867417fcab4238cbc3a
javi <narutojavibanda@gmail.com>
2021-07-07 00:34:13 +0200

    Change findByName for Optional<Category> object

--- src/main/java/com/example/myfirstapi/repository/CategoryRepository.java
+++ src/main/java/com/example/myfirstapi/repository/CategoryRepository.java
@@ -2,8 +2,9 @@ package com.example.myfirstapi.repository;
 import com.example.myfirstapi.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
-    List<Category> findByName(String name);
+    Optional<Category> findByName(String name);
 }
